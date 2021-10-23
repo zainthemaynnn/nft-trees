7206cd782b16f08a09df4bbddb3e2f1b36b8ff4f
zainthemaynnn <zainyusufazam@gmail.com>
2021-10-23 12:08:05 -0400

    add NFT-gen

---
+++ api/NFT-gen/species.js
@@ -0,0 +1,117 @@
+// this was painful to write by hand --zain
+// http://bhort.bh.cornell.edu/tree/list.htm
+var species;
+export default species = {
+  "Thuja": [
+    "Arborvitae",
+  ],
+  "Fraxinus": [
+    "Black Ash",
+    "White Ash",
+  ],
+  "Populus": [
+    "Bigtooth Aspen",
+    "Quaking Aspen",
+    "Eastern Cottonwood",
+  ],
+  "Tilia": [
+    "Basswood",
+  ],
+  "Fagus": [
+    "American Beech",
+  ],
+  "Betula": [
+    "Black Birch",
+    "Gray Birch",
+    "Paper Birch",
+    "Yellow Birch",
+  ],
+  "Juglans": [
+    "Butternut",
+  ],
+  "Prunus": [
+    "Black Cherry",
+    "Pin Cherry",
+  ],
+  "Castanea": [
+    "American Chestnut",
+  ],
+  "Magnolia": [
+    "Cucumber Tree",
+  ],
+  "Ulmus": [
+    "American Elm",
+    "Slippery Elm",
+  ],
+  "Abies": [
+    "Balsam Fir",
+  ],
+  "Crataegues": [
+    "Hawthorn",
+  ],
+  "Tsuga": [
+    "Eastern Hemlock",
+  ],
+  "Carya": [
+    "Bitternut Hickory",
+    "Pignut Hickory",
+    "Shagbark Hickory",
+  ],
+  "Ostrya": [
+    "American Hoperhornbeam",
+  ],
+  "Carpinus": [
+    "American Hornbeam",
+  ],
+  "Larix": [
+    "American Larch",
+  ],
+  "Robinia": [
+    "Black Locust",
+  ],
+  "Gleditsia": [
+    "Honey-Locust",
+  ],
+  "Acer": [
+    "Red Maple",
+    "Silver Maple",
+    "Sugar Maple",
+  ],
+  "Quercus": [
+    "Black Oak",
+    "Chestnut Oak",
+    "Northern Red Oak",
+    "Scarlet Oak",
+    "White Oak",
+  ],
+  "Pinus": [
+    "Eastern White Pine",
+    "Pitch Pine",
+    "Red Pine",
+  ],
+  "Juniperus": [
+    "Eastern Redcedar",
+  ],
+  "Sassafras": [
+    "Sassafras",
+  ],
+  "Amelanchier": [
+    "Shadbush",
+  ],
+  "Picea": [
+    "Red Spruce",
+    "White Spruce",
+  ],
+  "Platanus": [
+    "Sycamore",
+  ],
+  "Liriodendron": [
+    "Tulip Tree",
+  ],
+  "Juglans": [
+    "Black Walnut",
+  ],
+  "Salix": [
+    "Black Willow",
+  ],
+};
