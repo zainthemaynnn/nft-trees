c64917a6b53b1754f5bf413d6f6da5c4d30311a1
zainthemaynnn <zainyusufazam@gmail.com>
2021-10-23 08:02:13 -0400

    nft example

--- ui/src/conf/defaults.js
+++ ui/src/conf/defaults.js
@@ -1,17 +1,18 @@
-// GENERATED FROM /Users/katesills/code/dapp-card-store/api/deploy.js
+// GENERATED FROM /mnt/c/Users/Zain/awesomeness/hackathons/hack-this-fall-2021/api/deploy.js
 export default {
-  "INSTANCE_BOARD_ID": "794977354",
-  "INSTALLATION_BOARD_ID": "379944443",
-  "SELL_ITEMS_INSTALLATION_BOARD_ID": "1530295560",
-  "INVITE_BRAND_BOARD_ID": "1456154132",
+  "INSTANCE_BOARD_ID": "940953141",
+  "INSTALLATION_BOARD_ID": "1026677745",
+  "SELL_ITEMS_INSTALLATION_BOARD_ID": "931423413",
+  "INVITE_BRAND_BOARD_ID": "1265655452",
   "brandBoardIds": {
-    "Card": "529406044",
-    "Money": "1576549616"
+    "Card": "1168060102",
+    "Money": "1532665031"
   },
   "issuerBoardIds": {
-    "Card": "715805464",
-    "Money": "1667979430"
+    "Card": "925063544",
+    "Money": "1576549616"
   },
+  "pricePerCard": 1000000,
   "BRIDGE_URL": "http://127.0.0.1:8000",
   "API_URL": "http://127.0.0.1:8000",
   "CONTRACT_NAME": "cardStore"
--- ui/src/conf/installationConstants.js
+++ ui/src/conf/installationConstants.js
@@ -1,6 +1,6 @@
-// GENERATED FROM /Users/katesills/code/dapp-card-store/contract/deploy.js
+// GENERATED FROM /mnt/c/Users/Zain/awesomeness/hackathons/hack-this-fall-2021/contract/deploy.js
 export default {
   "CONTRACT_NAME": "cardStore",
-  "INSTALLATION_BOARD_ID": "379944443",
-  "SELL_ITEMS_INSTALLATION_BOARD_ID": "1530295560"
+  "INSTALLATION_BOARD_ID": "1026677745",
+  "SELL_ITEMS_INSTALLATION_BOARD_ID": "931423413"
 };
